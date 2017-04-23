43df895f5946c8f376ef28e3ae9ba757699dcbdd
Juan David Hernández <juan.hernandez@ecoservices.com.co>
2017-04-22 20:43:47 -0500

    - Agregados los grupos de animales faltantes junto con sus imágenes - Se mejoró la visualización de los íconos en la actividad principal - Se agregaron los nombres de los grupos de animales al archivo strings.xml para hacer caso a buenas prácticas

--- app/src/main/java/com/ingsoft/juandavids/farminfo/MainActivity.java
+++ app/src/main/java/com/ingsoft/juandavids/farminfo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ingsoft.juandavids.farminfo;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.ingsoft.juandavids.farminfo.utilities.AnimalAdapter;
@@ -9,7 +12,7 @@ import com.ingsoft.juandavids.farminfo.utilities.AnimalInfo;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
     GridView animalGrid;
     @Override
@@ -17,14 +20,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Resources res = this.getResources();
+        String[] animalStrings = res.getStringArray(R.array.animal_names);
+
         ArrayList<AnimalInfo> animalList = new ArrayList<>();
 
-        animalList.add(new AnimalInfo(R.drawable.bovinos, "Bovinos"));
-        animalList.add(new AnimalInfo(R.drawable.aves, "Aves"));
-        animalList.add(new AnimalInfo(R.drawable.caninos, "Caninos"));
-        animalList.add(new AnimalInfo(R.drawable.porcinos, "Porcinos"));
+        animalList.add(new AnimalInfo(R.drawable.aves, animalStrings[0]));
+        animalList.add(new AnimalInfo(R.drawable.ornamentales, animalStrings[7]));
+        animalList.add(new AnimalInfo(R.drawable.bovinos, animalStrings[1]));
+        animalList.add(new AnimalInfo(R.drawable.caninos, animalStrings[2]));
+        animalList.add(new AnimalInfo(R.drawable.caprinos, animalStrings[3]));
+        animalList.add(new AnimalInfo(R.drawable.conejos, animalStrings[4]));
+        animalList.add(new AnimalInfo(R.drawable.equinos, animalStrings[5]));
+        animalList.add(new AnimalInfo(R.drawable.felinos, animalStrings[6]));
+        animalList.add(new AnimalInfo(R.drawable.ovinos, animalStrings[8]));
+        animalList.add(new AnimalInfo(R.drawable.porcinos, animalStrings[9]));
 
         animalGrid = (GridView) findViewById(R.id.animalGridView);
         animalGrid.setAdapter(new AnimalAdapter(this, animalList));
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
